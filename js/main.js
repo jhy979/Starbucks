b0f98275cc6de423bce6fa6076f8c4a8b6385b1d
jhy979 <jhy979@gmail.com>
2021-08-22 16:20:47 +0900

    Visual Section 이미지 애니메이션 삽입

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// 검색창 
 const searchEl = document.querySelector('.search'); // input 요소랑 material-icons 전부 선택되겠죠
 const searchInputEl = searchEl.querySelector('input'); //input요소만 선택되겠죠
 
@@ -15,6 +16,7 @@ searchInputEl.addEventListener('blur', () => {
   searchInputEl.setAttribute('placeholder', ''); // HTML 속성을 지정하자
 })
 
+
 // 스크롤 시 배지 렌더링 조절
 const badgeEl = document.querySelector('header .badges')
 
@@ -23,13 +25,23 @@ window.addEventListener('scroll', _.throttle(() => {
   if (window.scrollY > 500) {
     gsap.to(badgeEl, .4, {
       opacity: 0,
-      display : 'none'
+      display: 'none'
     })
   } else {
     //배지 보이기
     gsap.to(badgeEl, .4, {
       opacity: 1,
-      display : 'block'
+      display: 'block'
     })
-  } 
+  }
 }, 300))
+
+
+// VISUAL section 이미지 시간 차 렌더링
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+fadeEls.forEach((el, idx) => {
+  gsap.to(el, 1, {
+    delay: (idx + 1) * .7,
+    opacity: 1,
+  })
+})
