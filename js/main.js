c3575f7f182146dddd8da09fffdabdd5200d6af0
jhy979 <jhy979@gmail.com>
2021-08-24 16:55:26 +0900

    프로모션 슬라이드 생성 및 토글 키 생성 --- slide page_prev,next키는 오류 상태

--- js/main.js
+++ js/main.js
@@ -47,10 +47,47 @@ fadeEls.forEach((el, idx) => {
 })
 
 
-//SWIPER
-// new Swiper(선택자, 옵션)
+//Notice SWIPER
+// new Swiper(선택자, 옵션) 
 new Swiper('.notice-line .swiper-container', {
   direction: 'vertical',
   autoplay: true,
   loop: true,
 });
+
+
+// Promotion SWIPER
+new Swiper('.promotion .swiper-container', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 5000
+  },
+  centeredSlides: true,
+  pagination: {
+    el: '.promotion .swiper-pagination', //페이지 번호 요소 선택자
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next',
+  }
+});
+
+
+//Toggle
+
+const promotionEl = document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', () => {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide')
+  } else {
+    promotionEl.classList.remove('hide')
+  }
+})
