1c145dda3166a3f4fcb416aa214ddb803b9ac44a
jhy979 <jhy979@gmail.com>
2021-08-26 14:54:43 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-// 검색창 
-const searchEl = document.querySelector('.search'); // input 요소랑 material-icons 전부 선택되겠죠
-const searchInputEl = searchEl.querySelector('input'); //input요소만 선택되겠죠
-
-searchEl.addEventListener('click', () => { //아무거나 눌러도
-  searchInputEl.focus(); // input 요소가 포커스 됩니다.
-})
-
-searchInputEl.addEventListener('focus', () => {
-  searchEl.classList.add('focused'); // 클래스 내용을 추가하자
-  searchInputEl.setAttribute('placeholder', '통합검색'); // HTML 속성을 지정하자
-})
-
-searchInputEl.addEventListener('blur', () => {
-  searchEl.classList.remove('focused'); // 클래스 내용을 추가하자
-  searchInputEl.setAttribute('placeholder', ''); // HTML 속성을 지정하자
-})
-
-
-
 // 스크롤 시 배지 렌더링 조절 
 // To Top
 const badgeEl = document.querySelector('header .badges')
@@ -155,7 +135,3 @@ new Swiper('.awards .swiper-container', {
     nextEl: '.awards .swiper-next'
   }
 })
-
-// 연도 자동 계산
-const thisYear = document.querySelector('.this-year')
-thisYear.textContent = new Date().getFullYear();
